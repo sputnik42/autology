549aa77cc613c46b5720c706002ba3f8c7780365
Robert Robinson <rerobins@gmail.com>
2017-11-07 16:21:45 -0700

    Add project maintenance files
    - setup.py to include requirements
    - Change license to MIT
    - Add LICENSE File
    - Stub out README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Autology
+
+Autology is a text file based life log.  Using the contents of markdown files (to include frontmatter) it will provide a 
+collection of reports for later viewing.
+
+## Installation
+
+```
+pip install git+https://github.com/MeerkatLabs/autology.git
+```
+
+## Contributing
+
+Please report all issues to [Bug tracker](https://github.com/MeerkatLabs/autology/issues)
+
+## Configuration
+
+> TODO
+
+## Execution
+
+> TODO
+
--- setup.py
+++ setup.py
@@ -2,13 +2,36 @@ from setuptools import find_packages, setup
 
 setup(
     name='autology',
-    version='0.1',
+    version='0.1.0.dev',
     packages=find_packages(),
     url='',
-    license='BSD 3 Clause',
+    license='MIT',
     author='Robert Robinson',
     author_email='rerobins@meerkatlabs.org',
     description='File-based life log',
+    python_requires='>=3.6, <4',
+    classifiers=[
+        'Environment :: Console',
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Text Processing'
+    ],
+    install_requires=[
+        'dict-recursive-update>=1.0.0,<2',
+        'Jinja2>=2.9.6,<3',
+        'Markdown>=2.6.9,<3',
+        'munch>=2.2.0,<3',
+        'Pypubsub>=4.0.0,<5',
+        'python-frontmatter>=0.4.2,<0.5',
+        'PyYAML>=3.12,<4',
+    ],
+
+    extras_require={
+        'dev': ['pylint>=1.7.4'],
+        'test': [],
+    },
+
     entry_points={
         'console_scripts': ['autology=autology.commands.main:main'],
         'autology_plugins': ['index_report=autology.reports.index:register_plugin',
