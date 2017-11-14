59bb66b2ccd038d187e24f58328ad6c9447273cf
Robert Robinson <rerobins@gmail.com>
2017-11-14 11:33:46 -0700

    Configure storage plugin
    Fixes #14
    
    - Read in configuration details from the configuration file
    - Initialize the git repository if it's not been done so already and
      the configuration says that it is enabled
    - Refactored the initialization method to test the remotes correctly

--- autology/storage.py
+++ autology/storage.py
@@ -5,44 +5,58 @@ git is the storage module, but there is not a reason why another storage impleme
 Current implementation assumes that git is initialized in the project directory, and has a remote named origin.
 """
 import git
-from autology.configuration import get_configuration_root
+from autology.configuration import get_configuration_root, add_default_configuration, get_configuration
 from autology import topics
 
 
 # The current repository that all of the files will be stored/modified in.
 _repo = None
 
-# Whether the remote repository is available for access or not
-_accessible = True
-
 
 def register_plugin():
     """Initializes the git module for the directory that the configuration file is currently being stored in."""
     topics.Application.INITIALIZE.subscribe(_initialization)
 
+    add_default_configuration('git', {
+        'enabled': False,
+        'remote': 'origin',
+        'refspec': 'master',
+    })
+
 
 def _initialization():
-    global _repo, _accessible
+    """Initialize storage engine by querying configuration, and then file system."""
+    global _repo
 
-    # If creating the git repository fails, then don't need to continue with this storage engine
+    configuration = get_configuration()
+
+    if not configuration.git.enabled:
+        return
+
+    # If creating the git repository fails, then initialize a new repository
     try:
         _repo = git.Repo(str(get_configuration_root()))
     except git.InvalidGitRepositoryError:
-        return
+        _repo = git.Repo.init(str(get_configuration_root()))
 
-    # Try to fetch from the origin remote in order to determine if the remote repository is active
     try:
-        _repo.remotes.origin.fetch()
-        _accessible = True
-    except (git.GitCommandError, AttributeError):
+        # Add the subscriptions for working with local storage
+        topics.Storage.FILE_ADDED.subscribe(_file_added)
+        topics.Storage.FINISHED_MODIFICATIONS.subscribe(_finished_changes)
+
+        # Test status of remote accessibility
+        if configuration.git.remote:
+            origin_remote = _repo.remote(name=configuration.git.remote)
+            origin_remote.fetch()
+
+            # And then add subscriptions for handling remotes
+            topics.Storage.PULL_CHANGES.subscribe(_pull_changes)
+            topics.Storage.PUSH_CHANGES.subscribe(_push_changes)
+
+    except (git.GitCommandError, ValueError):
         # Command error is raised when the remote is not accessible and attribute error is raised when the remote is not
         # defined.
-        _accessible = False
-
-    topics.Storage.FILE_ADDED.subscribe(_file_added)
-    topics.Storage.FINISHED_MODIFICATIONS.subscribe(_finished_changes)
-    topics.Storage.PULL_CHANGES.subscribe(_pull_changes)
-    topics.Storage.PUSH_CHANGES.subscribe(_push_changes)
+        pass
 
 
 def _file_added(file):
@@ -57,11 +71,11 @@ def _finished_changes(message):
 
 def _pull_changes():
     """Pull changes from the repository and do a merge"""
-    if _accessible:
-        _repo.remotes.origin.pull('master')
+    configuration = get_configuration()
+    _repo.remotes[configuration.git.remote].pull(configuration.git.refspec)
 
 
 def _push_changes():
     """Push changes to the repository."""
-    if _accessible:
-        _repo.remotes.origin.push('master')
+    configuration = get_configuration()
+    _repo.remotes[configuration.git.remote].push(configuration.git.refspec)
