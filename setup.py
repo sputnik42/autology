2b68a722f6c7b48efe80e1c34404ad69dfa6c6d6
Robert Robinson <rerobins@gmail.com>
2017-12-19 16:03:58 -0700

    Update version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='autology',
-    version='0.2.0.dev',
+    version='0.2.0',
     packages=find_packages(),
     url='',
     license='MIT',
